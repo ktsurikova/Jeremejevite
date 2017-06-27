54ebc82ef128bc156994e3450b9efeec62d82d57
KIA <kionopia@gmail.com>
2017-06-27 14:02:41 +0300

    non-recursive merge&quicksort

---
+++ NET.W.2016.01.Tsurikova.01/ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sortings;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unsortedQ = { 12, 11, 17, 8, 7, 11, 11, 9, 4 };
+            int[] unsortedM = { 12, 11, 17, 8, 7, 11, 11, 9, 4 };
+            Array.ForEach(unsortedQ, i => { Console.Write("{0} ", i); });
+            Sorter.QuickSort(unsortedQ);
+            Console.WriteLine("\nQuickSort");
+            Array.ForEach(unsortedQ, i => { Console.Write("{0} ", i); });
+            Sorter.MergeSort(unsortedM);
+            Console.WriteLine("\nMergeSort");
+            Array.ForEach(unsortedM, i => { Console.Write("{0} ", i); });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NET.W.2016.01.Tsurikova.01/Sortings/Sorter.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortings
+{
+    /// <summary>
+    /// provides methods for sorting arrays
+    /// </summary>
+    public class Sorter
+    {
+        #region QuickSort
+
+        /// <summary>
+        /// sorts an array using non-recursive quicksort algorithm
+        /// </summary>
+        /// <param name="array">array to be sorted</param>
+        /// <exception cref="ArgumentNullException">when array is null</exception>>
+        public static void QuickSort(int[] array)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException("array is null");
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            stack.Push(array.Length);
+
+            while (stack.Count != 0)
+            {
+                int end = stack.Pop();
+                int start = stack.Pop();
+                if (end - start < 2) { continue; }
+
+                int p = Partition(array, start, end - 1);
+
+                stack.Push(p + 1);
+                stack.Push(end);
+                stack.Push(start);
+                stack.Push(p);
+            }
+        }
+
+        private static int Partition(int[] input, int start, int end)
+        {
+            int position = start + (end - start) / 2;
+            int piv = input[position];
+            Swap(ref input[position], ref input[end]);
+
+            int index = start;
+
+            for (int i = start; i < end; i++)
+            {
+                if (input[i] <= piv)
+                {
+                    Swap(ref input[i], ref input[index++]);
+                }
+            }
+
+            Swap(ref input[index], ref input[end]);
+            return index;
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        #endregion
+
+        #region MergeSort
+
+        /// <summary>
+        /// sorts an array using non-recursive mergesort algorithm
+        /// </summary>
+        /// <param name="array">array to be sorted</param>
+        /// <exception cref="ArgumentNullException">when array is null</exception>>
+        public static void MergeSort(int[] array)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException("array is null");
+            if (array.Length < 2) return;
+
+            int step = 1;
+            int startL, startR;
+
+            while (step < array.Length)
+            {
+                startL = 0;
+                startR = step;
+                while (startR + step <= array.Length)
+                {
+                    MergeParts(array, startL, startL + step, startR, startR + step);
+
+                    startL = startR + step;
+                    startR = startL + step;
+                }
+
+                //if an array of odd length
+                if (startR < array.Length)
+                {
+                    MergeParts(array, startL, startL + step, startR, array.Length);
+                }
+                step *= 2;
+            }
+        }
+
+        private static void MergeParts(int[] array, int startL, int stopL,
+            int startR, int stopR)
+        {
+
+            int[] right = new int[stopR - startR];
+            int[] left = new int[stopL - startL];
+
+            Array.Copy(array, startR, right, 0, right.Length);
+            Array.Copy(array, startL, left, 0, left.Length);
+
+            for (int k = startL, m = 0, n = 0; k < stopR; ++k)
+            {
+                if (n == right.Length)
+                {
+                    array[k] = left[m++];
+                    continue;
+                }
+                if (m == left.Length)
+                {
+                    array[k] = right[n++];
+                    continue;
+                }
+                if (left[m] <= right[n])
+                {
+                    array[k] = left[m++];
+                }
+                else
+                {
+                    array[k] = right[n++];
+                }
+            }
+        }
+
+        #endregion
+    }
+}
